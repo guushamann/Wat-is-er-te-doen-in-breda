8862c08cea71a014e054bfffbd2f7b20c17a7446
Guus Hamann <info@gustafson.nl>
2025-01-26 22:45:55 +0100

    add pwa

---
+++ public/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "astro-basics-cache-v1";
+const urlsToCache = [
+  "/",
+  "/manifest.json",
+  "/favicon.svg",
+  "/pwa-192x192.png",
+  "/pwa-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
