95c9c07f8169af619e27bdfea6747acc419099ef
Guus Hamann <info@gustafson.nl>
2025-02-01 19:18:36 +0100

    styling

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,23 +13,6 @@ export default {
             "--tw-prose-headings": "var(--theme-text)",
             "--tw-prose-links": "var(--theme-text)",
             "--tw-prose-bold": "var(--theme-text)",
-            "h1, h2, h3, h4, h5, h6": {
-              fontWeight: "200",
-              fontFamily: "Funnel Display",
-              marginBottom: "0",
-              marginTop: "0",
-              lineHeight: "0.7",
-              fontSize: "2rem",
-            },
-            h1: {
-              fontSize: "5rem",
-            },
-            h2: {
-              fontSize: "3rem",
-            },
-            h3: {
-              fontSize: "2rem",
-            },
           },
         },
       }),
